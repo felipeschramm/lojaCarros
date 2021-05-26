5b589a095743eaa4bfe39349138dbb9ebeead3ba
Felipe <felipecschramm@hotmail.com>
2021-05-26 10:54:40 -0300

    desafio 31

--- index.js
+++ index.js
@@ -2,29 +2,16 @@
     'use strict';
 
     /*
-A loja de carros será nosso desafio final. Na aula anterior, você fez a parte
-do cadastro dos carros. Agora nós vamos começar a deixar ele com cara de
-projeto mesmo.
-Crie um novo repositório na sua conta do GitHub, com o nome do seu projeto.
-Na hora de criar, o GitHub te dá a opção de criar o repositório com um
-README. Use essa opção.
-Após criar o repositório, clone ele na sua máquina.
-Crie uma nova branch chamada `challenge-30`, e copie tudo o que foi feito no
-desafio da aula anterior para esse novo repositório, nessa branch
-`challenge-30`.
-Adicione um arquivo na raiz desse novo repositório chamado `.gitignore`.
-O conteúdeo desse arquivo deve ser somente as duas linhas abaixo:
-node_modules
-npm-debug.log
-Faça as melhorias que você achar que são necessárias no seu código, removendo
-duplicações, deixando-o o mais legível possível, e então suba essa alteração
-para o repositório do seu projeto.
-Envie um pull request da branch `challenge-30` para a `master` e cole aqui
-nesse arquivo, dentro do `console.log`, o link para o pull request no seu
-projeto.
-*/
+    Agora vamos criar a funcionalidade de "remover" um carro. Adicione uma nova
+    coluna na tabela, com um botão de remover.
+    Ao clicar nesse botão, a linha da tabela deve ser removida.
+    Faça um pull request no seu repositório, na branch `challenge-31`, e cole
+    o link do pull request no `console.log` abaixo.
+    Faça um pull request, também com a branch `challenge-31`, mas no repositório
+    do curso, para colar o link do pull request do seu repo.
+    */
 
-    console.log('Link do pull request do seu projeto');
+    console.log('Link do pull request do seu projeto: ');
 
     function app(DOM) {
         return {
@@ -37,10 +24,9 @@ projeto.
                 var $table = document.querySelector('[data-js="table"]');
 
                 function getInfo() {
-                    ajax.open("GET", "./company.json", true);
+                    ajax.open("GET", "./company.json");
                     ajax.send();
                 }
-                getInfo();
 
                 ajax.addEventListener('readystatechange', function () {
 
@@ -66,8 +52,33 @@ projeto.
                         td.textContent = info.value;
                         tr.appendChild(td);
                     });
+                    tr.appendChild(addButtonRemove())
                     $table.appendChild(tr);
+                    cleanFields()
                 }
+
+                function cleanFields() {
+                    $info.forEach(info => {
+                        info.value = ''
+                    })
+                }
+
+                function addButtonRemove() {
+                    var tdButton = document.createElement("td");
+                    var buttonRemove = document.createElement('button')
+                    buttonRemove.textContent = 'Remover'
+
+                    tdButton.appendChild(buttonRemove)
+
+                    buttonRemove.onclick = function () {
+                        var row = tdButton.parentNode;
+                        row.parentNode.removeChild(row)
+                    }
+
+                    return tdButton;
+
+                }
+                getInfo();
             }
         }
 
